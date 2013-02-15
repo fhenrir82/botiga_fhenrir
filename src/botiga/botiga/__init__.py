123a1242504d32088842063cbd0ac2e3a507e816
Aleix <aleix@aleix-laptop.(none)>
2013-02-15 20:01:46 +0100

    canviat l'__init__.py per canviar la ruta de la pagina

--- src/botiga/botiga/__init__.py
+++ src/botiga/botiga/__init__.py
@@ -11,8 +11,8 @@ def main(global_config, **settings):
     #amb aquesta linia serveix per no tenir problemes amb els templates
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('benvinguda','/botiga') #productes=view, /botiga=URL
-    config.add_route('productes','/botiga/productes')
+    config.add_route('benvinguda', '/')
+    #config.add_route('benvinguda','/botiga') #productes=view, /botiga=URL
+    config.add_route('productes','/productes')
     config.scan()
     return config.make_wsgi_app()
