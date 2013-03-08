6f704746a4262889fd44bd8b97233313787be0bd
Aleix <aleix@aleix-laptop.(none)>
2013-03-08 20:42:20 +0100

    actualitzacio

--- src/botiga/botiga/productes.py
+++ src/botiga/botiga/productes.py
@@ -11,3 +11,16 @@ def Productes():
         diccionari.update({id:{'producte':producte, 'unitats':unitats, 'preu':preu}})
     prods.close()
     return diccionari
+    
+def Dades():
+	diccionari={}
+	fitxer=here+'/comanda.txt'
+	comanda=open(fitxer,'a')
+	for linia in comanda:
+		fitxer.write('01')
+		fitxer.write(' ')
+		fitxer.write(llista_valors[0])
+		fitxer.write(' ')
+		fitxer.write(llista_valors[1])
+		fitxer.write('\n')
+	
