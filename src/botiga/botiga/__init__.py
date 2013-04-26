4a4547d0b8528dbddbf247149799154c1026e2f6
Aleix <aleix@aleix-laptop.(none)>
2013-04-26 17:34:24 +0200

    actualitzada la botiga per a l'autentificacio cutre

--- src/botiga/botiga/__init__.py
+++ src/botiga/botiga/__init__.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
 from pyramid.config import Configurator
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+
+from .security import groupfinder    
+from .models import RootFactory
+
 import os
+here = os.path.dirname(os.path.abspath(__file__))
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    here = os.path.dirname(os.path.abspath(__file__))
-    #el here serveix per a poder saber a quina ruta es troba l'arxiu
+    
     settings['mako.directories'] = os.path.join(here, 'templates')
+        # afegit del auth module
+    authn_policy = AuthTktAuthenticationPolicy(
+        'sosecret', callback=groupfinder, hashalg='sha512')
+    authz_policy = ACLAuthorizationPolicy()
+    config = Configurator(root_factory='.models.RootFactory', settings=settings)
+    #config = Configurator(settings=settings)
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
+    #el here serveix per a poder saber a quina ruta es troba l'arxiu
+    
     #amb aquesta linia serveix per no tenir problemes amb els templates
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
@@ -15,5 +31,7 @@ def main(global_config, **settings):
     config.add_route('home', '/home')
     #config.add_route('benvinguda','/botiga') #productes=view, /botiga=URL
     config.add_route('productes','/productes')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
     config.scan()
     return config.make_wsgi_app()
--- src/botiga/botiga/views.py
+++ src/botiga/botiga/views.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
-from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+from pyramid.view import (
+    view_config,
+    forbidden_view_config,
+)
+from pyramid.security import (
+    remember,
+    forget,
+    authenticated_userid,
+)
+from .security import comprova_usuari
+
 from productes import Productes
 # from fitxer import funcio
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/mytemplate.pt', permission='admin')
 def my_view(request):
-    return {'project':'botiga'}
+    return {'project':'botiga','logged_in':authenticated_userid(request)}
 
-@view_config(route_name='benvinguda', renderer='benvinguts.mako')
+@view_config(route_name='benvinguda', renderer='benvinguts.mako', permission='public')
 def benvinguda_view(request):
     missatge = "Aleix's Shop"
-    return{"miss":missatge}
-@view_config(route_name='productes', renderer='productes.mako')
+    return{"miss":missatge,'logged_in':authenticated_userid(request)}
+@view_config(route_name='productes', renderer='productes.mako', permission='admin')
 def productes_view(request):
     pros=Productes()
-    return {"products":pros}
+    return {"products":pros,'logged_in':authenticated_userid(request)}
    # aqui aniriem als arxius o la base de dades a buscar la informació
    # així ho simulem, manera alternativa cutre
    #proj = "Botigueta Pro"
@@ -22,3 +34,49 @@ def productes_view(request):
    #preus = { 'pepino':'2€/kg', 'enciam':'1€/peça', 'plàtan':'2.5€/kg' }
    # els retornarem amb:
    #return { "projecte":proj, "productes":prod, "preus":preus }
+
+
+# aquest decorator és per establir la ruta per /login
+@view_config( route_name='login', renderer='login.mako')
+# aquest altre ens redirigirà aquí quan algú intenti entrar en una web que no té permís
+@forbidden_view_config(renderer='login.mako')
+def login(request):
+    login_url = request.route_url('login')
+    # detectem des de quina URL ve el visitant
+    referrer = request.url
+    # retornem l'usuari a la home page si ha vingut directe al login
+    if referrer == login_url:
+        referrer = '/' # never use the login form itself as came_from
+    came_from = request.params.get('came_from', referrer)
+    user = authenticated_userid(request)
+    if user:
+        lloc = came_from.split("/")
+        message = "Ets %s, i com a tal no pots entrar a %s" % (user,lloc[len(lloc)-1])
+    else:
+        message = "Identifica't per entrar a la Botiga"
+    login = ''
+    password = ''
+    if 'form.submitted' in request.params:
+        login = request.params['login']
+        password = request.params['password']
+        if comprova_usuari(login,password):
+            headers = remember(request, login)
+            return HTTPFound(location = came_from,
+                             headers = headers)
+        message = 'Failed login'
+
+    return dict(
+        message = message,
+        url = request.application_url + '/login',
+        came_from = came_from,
+        login = login,
+        password = password,
+        user = authenticated_userid(request), # afegim usuari autenticat si l'hi ha
+        )
+    
+
+@view_config(route_name='logout')
+def logout(request):
+    headers = forget(request)
+    return HTTPFound(location = request.route_url('benvinguda'),
+                     headers = headers)
